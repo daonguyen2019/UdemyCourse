4d89a9859d8c7c03da4d290b0e4417645f17b59a
daonguyen2019 <57763158+daonguyen2019@users.noreply.github.com>
2023-07-03 21:23:14 +0700

    Update and rename matplotlib to matplotlib.py

---
+++ matplotlib.py
@@ -0,0 +1,10 @@
+
+def createchartmultipleline(title, x,xlabel, dict_y, figsize=(8,6), style='fivethirtyeight'):
+    plt.style.use(style)
+    plt.figure(figsize=figsize)
+    plt.title(title)
+    for key in dict_y.keys():
+        plt.plot(x,dict_y[key], label = key)
+    plt.xlabel(xlabel)
+    plt.legend()
+    plt.show()
