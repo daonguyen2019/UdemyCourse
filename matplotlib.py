e7c68133210abb6729db78c376db87b82a36e9d2
daonguyen2019 <57763158+daonguyen2019@users.noreply.github.com>
2023-07-18 22:22:50 +0700

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -6,6 +6,7 @@ def createchartmultipleline(title, x,xlabel, dict_y, figsize=(8,6), style='fivet
         plt.plot(x,dict_y[key], label = key)
     plt.xlabel(xlabel)
     plt.legend(shadow = True, frameon = True, facecolor = 'white')
+
     plt.show()
 
 plt.bar(x, y, width=, color  = , align= 'edge')
@@ -26,6 +27,7 @@ plt.yticks([list of bin in col],labels = [])
 plt.xlim()
 plt.ylim()
 plt.legend()
-
+plt.legend(bbox_to_anchor = (1.05,1) #set legend outside of the box)
+           
 plt.hist(data, label = , alpha = )
 
