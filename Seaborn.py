ed3b8465a4f0b68c9e3f33d705eeff85a3bf6245
daonguyen2019 <57763158+daonguyen2019@users.noreply.github.com>
2023-07-18 20:45:55 +0700

    Add files via upload

---
+++ Seaborn.py
@@ -0,0 +1,36 @@
+#https://matplotlib.org/stable/tutorials/colors/colormaps.html
+
+import Seaborn as sns
+#Scatter plots
+sns.scatterplot(x= colName
+                ,y= colName
+                , data = df
+                , hue = colName
+                , palette = 
+                , size = colName
+                , alpha = (0,1)/transparency
+                , style = colName
+                )
+plt.savefig(name.jpg)
+
+#Distribution Plots
+sns.set(style = 'darkgrid')
+sns.displot(data=df
+            ,x='salary'
+            ,bins= 
+            , kde = True
+ )
+sns.kdeplot(
+    data = 
+    , x=''
+    , clip = []
+    , bw_adjust = 0.3
+    , shade = True #fill in shape
+ )
+
+
+
+#Categorical Plots
+#Comparison Plots
+#Seaborn Grids
+#Matrix Plots
